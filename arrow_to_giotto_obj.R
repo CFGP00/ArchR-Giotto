646f47fa0550f142cfdc7fa84ef4f79a4fc29670
Sanjana Hasyagar <157615573+shasyagar@users.noreply.github.com>
2025-04-08 23:59:59 -0400

    Update arrow_to_giotto_obj.R
    added spatial co-ords

--- arrow_to_giotto_obj.R
+++ arrow_to_giotto_obj.R
@@ -16,10 +16,14 @@ setnames(spatial_data, old = c("V1", "V2", "V3", "V4", "V5", "V6"),
          new = c("barcode", "in_tissue", "array_row", "array_col", "x_spatial", "y_spatial"))
 run_id <- 'D01942_NG05827'
 spatial_data[, barcode := paste0(run_id, "#", barcode, "-1")]
+
 # Get on-tissue barcodes (16,314)
 filtered_barcodes <- spatial_data[in_tissue == 1]$barcode
 
 proj_filtered <- proj[proj$cellNames %in% filtered_barcodes]
+
+# Create spatial co-ords with only the barcode,x and y values
+spatial_coords <- spatial_data[barcode %in% proj_filtered$cellNames, .(barcode, x_spatial, y_spatial)]
 
 # Get the gene score matrix
 geneScoreMatrix <- getMatrixFromProject(proj_filtered, useMatrix = "GeneScoreMatrix")
